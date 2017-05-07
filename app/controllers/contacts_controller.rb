38afeb96a7bb33d6dda54dae97944e8b7840b554
Aleksander Wozniak <4wozniak@gmail.com>
2017-05-07 15:06:12 +0000

    Dodano komentarze do Contacts Controller oraz Contact.rb

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,18 +1,33 @@
 class ContactsController < ApplicationController
+    
+    # GET request do /contact-us
+    # Pokazuje nowy formularz kontaktowy - name/mail/comm
     def new
         @contact = Contact.new  
     end
     
+    # POST request do /contacts - wynika z routingu
     def create
+        # Mass assignment - masowe przypisanie pól formularza do obiektu Contact
         @contact = Contact.new(contact_params)
+        # Zapisz obiekt Contact do bazy danych
         if @contact.save
+            # Zachowaj dane z formularza
+            # Store form fields via parameters into variables
             name = params[:contact][:name]
             email = params[:contact][:email]
             body = params[:contact][:comments]
+            # Wstaw zmienne (variables) do Contact Mailer,
+            # do metody email i wyślij email
             ContactMailer.contact_email(name, email, body).deliver
+            # Wiadomość o sukcesie w hashu flash
+            # i przenieś do nowej akcji
             flash[:success] = "Message sent."
             redirect_to new_contact_path
         else
+            # Jesli obiekt Contact sie nie zapisze,
+            # zapisz errory do hasha FLASH
+            # i redirectuj do new action
             flash[:danger] = @contact.errors.full_messages.join(", ")
             redirect_to new_contact_path
         end
@@ -20,6 +35,9 @@ class ContactsController < ApplicationController
     
     
     private 
+        # Aby zebrać dane z formularza musimy użyć 
+        # strong parameters i whitelistować pola formularza.
+        # use strong parameters and whitelist the form fields. [Security]
         def contact_params
             params.require(:contact).permit(:name, :email, :comments)
         end
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,4 +1,5 @@
 class Contact < ActiveRecord::Base
+    # Wymagania do formularza Contact form
     validates :name, presence: true
     validates :email, presence: true
     validates :comments, presence: true
