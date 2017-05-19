491606af10f440b64c6f9e3962ae130777955de6
Aleksander Wozniak <4wozniak@gmail.com>
2017-05-19 13:02:51 +0000

    Upgraded form validation for new profile and upgraded phone number format to include spaces and dashes

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -4,6 +4,7 @@ class Profile < ActiveRecord::Base
                     :styles => { :medium => "300x300>", :thumb => "100x100>" },
                     :default_url => "/images/:style/missing.png"
     validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
-    validates :phone_number, :presence => true, :numericality => true,
-                             :length => { :minimum =>8, :maximum => 15 }
+    validates :phone_number, :presence => true,
+                             :length => { :minimum =>8, :maximum => 16 },
+                             format: { with: /\d[0-9]-?\d[0-9]-?\d[0-9]/ }
 end
